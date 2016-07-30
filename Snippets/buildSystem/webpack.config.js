4ebc9ac6b3a5f086fbffe72d1eae9c85b321315b
mlnck <jmelnick@jmelnick.com>
2016-07-30 11:48:55 -0400

    Generic Build System Initial Commit

---
+++ Snippets/buildSystem/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path'),
+	webpack = require('webpack'),
+	packageData = require('./package.json'),
+	minify = process.argv.indexOf('--minify') != -1,
+	filename = [packageData.name, packageData.version, 'js'],
+	plugins = [];
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = {
+    entry: path.resolve(__dirname, packageData.main),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: filename.join('.'),
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel',
+          query: {
+            presets: ['es2015']
+          }
+        }
+      ]
+    },
+    plugins: plugins
+}
