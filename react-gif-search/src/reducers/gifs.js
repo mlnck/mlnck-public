454576a412167bc64c7e7e5c6ce2f9e9e5064f53
mlnck <jmelnick@jmelnick.com>
2017-03-15 05:17:31 -0400

    Minor, obj formatting

--- react-gif-search/src/reducers/gifs.js
+++ react-gif-search/src/reducers/gifs.js
@@ -8,7 +8,8 @@ export default function gifs(state = initialState, action) {
   switch (action.type) {
     case REQUEST_GIFS:
       return {
-        ...state, data: action.payload.body.data
+        ...state,
+        data: action.payload.body.data
       };
     default:
       return state;
