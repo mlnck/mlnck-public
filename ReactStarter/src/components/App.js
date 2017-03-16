310909979e188a210b45b11a687daef86b761052
mlnck <jmelnick@jmelnick.com>
2017-03-16 01:38:12 -0400

    Updates for React Base

--- ReactStarter/src/components/App.js
+++ ReactStarter/src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import Header from '../containers/Header';
 
 export default class App extends React.Component
 {
   render(){
     return(
       <div>
-        // <Header />
         {this.props.children}
       </div>
     );
