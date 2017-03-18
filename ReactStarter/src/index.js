1c7c4d4fbff0d5842bb2e57751a3eca7e1706459
mlnck <jmelnick@jmelnick.com>
2017-03-17 23:56:49 -0400

    Starter Files Updated

--- ReactStarter/src/index.js
+++ ReactStarter/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Home from './containers/Home';
+// import RequireAuth from './containers/RequireAuth';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
 import './styles/app.css';
 
+const store = configureStore();
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app-root')
 );
