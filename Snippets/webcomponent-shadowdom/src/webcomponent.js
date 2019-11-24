661beb131119e4f5939512327d41a8d470609c33
mlnck <jmelnick@jmelnick.com>
2019-11-24 11:35:19 -0500

    Added defined callback

--- Snippets/webcomponent-shadowdom/src/webcomponent.js
+++ Snippets/webcomponent-shadowdom/src/webcomponent.js
@@ -1,26 +1,31 @@
 (function(){
-  let tmpl = document.createElement('template');
-      tmpl.innerHTML = `<style>:host{}</style><p>shadow dom</p><slot></slot>`;
+  let tmpl = document.createElement('template')
+      tmpl.innerHTML = `<style>:host{}</style><p>shadow dom</p><slot></slot>`
   class WebComponent extends HTMLElement
   {
     constructor() {
-      super();
-      let shadowRoot = this.attachShadow({mode: 'open'});
-      shadowRoot.appendChild(tmpl.content.cloneNode(true));
+      super()
+      let shadowRoot = this.attachShadow({mode: 'open'})
+      shadowRoot.appendChild(tmpl.content.cloneNode(true))
     }
     connectedCallback()
-    { console.log('connected') }
+    {
+      console.log('connected')
+      customElements.whenDefined('web-component').then(() => {
+        console.log('defined') // add nested shadow elements after defined
+      })
+    }
     disconnectedCallback()
     { console.log('disconnected') }
 
     static get observedAttributes() 
     { return [/*attrName*/] } // return destructured list of attributes to listen to
     /*
-      get attrName() { return this.hasAttribute('attrName'); }
+      get attrName() { return this.hasAttribute('attrName') }
       set attrName(val) { (val) ? this.setAttribute('disabled', '') : this.removeAttribute('disabled') }
     */
     attributeChangedCallback(attrName, oldVal, newVal)
     {/* handle side effects only - get/set for logic */}
   }
-  window.customElements.define('web-component', WebComponent);
+  window.customElements.define('web-component', WebComponent)
 })()
